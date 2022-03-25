117fa69a1593ce72856020e1efef1034b7661343
jibisheng <jlc342501@gmail.com>
2022-03-25 17:02:59 +0800

    添加季毕胜饼状图

--- src/views/dashboard/Observe/Catory/index.vue
+++ src/views/dashboard/Observe/Catory/index.vue
@@ -20,34 +20,87 @@
 </template>
 
 <script>
+import * as echarts from "echarts";
 export default {
   data() {
     return {
-      radio1:'学习'
-    }
+      radio1: "学习",
+    };
   },
   mounted() {
-    let mychart=echarts.init(this.$refs.charts);
+    let mychart = echarts.init(this.$refs.charts);
     mychart.setOption({
+      title:{
+        text:"学习",
+        subtext:1048,
+        left:'center',
+        top:'center'
+      },
+      tooltip: {
+        trigger: "item",
+      },
       
+      series: [
+        {
+          name: "Access From",
+          type: "pie",
+          radius: ["40%", "70%"],
+          avoidLabelOverlap: false,
+          itemStyle: {
+            borderRadius: 10,
+            borderColor: "#fff",
+            borderWidth: 2,
+          },
+          label: {
+            show: true,
+            position: "outside",
+          },
+          emphasis: {
+            label: {
+              show: true,
+              fontSize: "40",
+              fontWeight: "bold",
+            },
+          },
+          labelLine: {
+            show: true,
+          },
+          data: [
+            { value: 1048, name: "学习" },
+            { value: 735, name: "购物" },
+            { value: 580, name: "游戏" },
+            { value: 484, name: "陪对象" },            
+          ],
+        },
+      ],
+    });
+    mychart.on('mouseover',  (params)=>{
+      const {value ,name}=params.data;
+      mychart.setOption({
+        title:{
+          text:name,
+          subtext:value,
+        }
+      })
     })
+    
+
   },
 };
 </script>
 
 <style scoped>
-  .catogory-header{
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-  }
-  .header{
-    border-bottom: 1px solid #eee;
-    padding: 5px 0;
-  }
-  .charts{
-    width: 100%;
-    height: 300px;
-  }
-
+.catogory-header {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+}
+.header {
+  border-bottom: 1px solid #eee;
+  padding: 5px 0;
+}
+.charts {
+  width: 100%;
+  height: 300px;
+}
 </style>
